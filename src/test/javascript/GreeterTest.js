61b0e2c276311d3d9f21fcdc493fd8990c39bbfc
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2012-07-12 11:38:28 +0200

    oups: test failed

--- src/test/javascript/GreeterTest.js
+++ src/test/javascript/GreeterTest.js
@@ -4,3 +4,8 @@ GreeterTest.prototype.testGreet = function() {
   var greeter = new myapp.Greeter();
   assertEquals("Hello World!", greeter.greet("World"));
 };
+
+GreeterTest.prototype.testGreetFoo = function() {
+  var greeter = new myapp.Greeter();
+  assertEquals("Hello foo!", greeter.greet("foo"));
+};
