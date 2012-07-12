5a3765e02f16e40cf4bcee6d66d51553d6110a1d
Nicolas Bétheuil <nicolas.betheuil@valtech.fr>
2012-07-12 11:35:35 +0200

    add: special case not tested, so coverage go down

--- src/main/javascript/Greeter.js
+++ src/main/javascript/Greeter.js
@@ -3,5 +3,8 @@ myapp = {};
 myapp.Greeter = function() { };
 
 myapp.Greeter.prototype.greet = function(name) {
+  if (name === "foo") {
+      name = name + "bar qix";
+  }
   return "Hello " + name + "!";
 };
